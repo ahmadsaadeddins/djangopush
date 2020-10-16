aff7fa5766f9feb52976cffec237a04f0208bd16
Ahmad Saadeddin <AS@ahm4d.com>
2020-10-16 20:36:51 +0200

    Service Worker

--- djangopush/djangopush/urls.py
+++ djangopush/djangopush/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from .views import home, send_push
 
@@ -10,4 +11,8 @@ urlpatterns = [
     path('', home),
     path('send_push', send_push),
     path('webpush/', include('webpush.urls')),
+    path(
+        'sw.js',
+        TemplateView.as_view(template_name='sw.js',
+                             content_type='application/x-javascript'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
