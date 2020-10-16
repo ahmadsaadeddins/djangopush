33750b52f1836e48f23c9a09de6867e711799ecb
Ahmad Saadeddin <AS@ahm4d.com>
2020-10-16 20:01:03 +0200

    WEBPUSH_SETTINGS & views & urls

--- djangopush/djangopush/settings.py
+++ djangopush/djangopush/settings.py
@@ -3,7 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -14,7 +13,6 @@ SECRET_KEY = 'y05#6_lz*)^aw#b-#6!&qy)3wj(8i!)4@4&7f(2xjiy+o@2#25'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -58,7 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangopush.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -69,25 +66,27 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -102,8 +101,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+WEBPUSH_SETTINGS = {
+    "VAPID_PUBLIC_KEY":
+    "BKj44jNqdaXbQvvtrPFlI5J8V9F1veBQo7WxyJAJ7whqcJPPtegFqNxpCYak_IUcczpy6cUWJspw9qOVSu7NUAY",
+    "VAPID_PRIVATE_KEY": "j8OJrlS9lbWdJh2Te_wwaxLQQTC89ivxS4tUWIdymNc",
+    "VAPID_ADMIN_EMAIL": "as@ahm4d.com"
+}
